43e68d8a47f79bc9d76015f403543bb7d41936d3
Michael Loo <michael.loo@rea-group.com>
2025-05-22 10:18:14 +1000

    Add AppboyPushStory via SPM

---
+++ NotificationExtension/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  NotificationExtension
+//
+//  Created by Michael Loo on 22/5/2025.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
