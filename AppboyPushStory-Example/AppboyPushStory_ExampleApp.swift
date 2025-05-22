809bacab2a3e45eddca87a1021dc4c598ed7d2ff
Michael Loo <michael.loo@rea-group.com>
2025-05-22 10:14:38 +1000

    Initial Commit

---
+++ AppboyPushStory-Example/AppboyPushStory_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppboyPushStory_ExampleApp.swift
+//  AppboyPushStory-Example
+//
+//  Created by Michael Loo on 22/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppboyPushStory_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
